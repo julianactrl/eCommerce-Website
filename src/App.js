de94d0b0a9fd186a840b73f5dfeb64e5c34f6901
julianactrl <juliana.mg3@gmail.com>
2020-12-12 15:43:40 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react'
-
+import Header from './components/Header'
+import './default.scss'
 
 
 function App() {
   return (
     <div >
+     <Header />
      <h1>Hola mundo React</h1>
-     <Contador />
     </div>
   );
 }
